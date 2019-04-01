b1d6892293f3f961eb2fcb6fb248e00b91b4e741
marb61a <marb61a@gmail.com>
2019-04-01 16:29:03 +0100

    added initial header component files

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+// import { AuthService } from '../../auth/shared/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: "bwm-header",
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.scss"]
+})
+
+export class HeaderComponent {
+    // constructor(private auth: AuthService, private router: Router) {}
+
+}
