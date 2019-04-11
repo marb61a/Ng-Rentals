2b11eadd12edc257380269b198164b96b068feb9
marb61a <marb61a@gmail.com>
2019-04-11 16:02:13 +0100

    added initial server folder and user model

---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        min: [4, 'Too short, min is 4 characters'],
+        max: [32, 'Too long, max is 32 characters']
+    },
+    email: {
+        type: String,
+        min: [4, 'Too short, min is 4 characters'],
+        max: [32, 'Too long, max is 32 characters'],
+        unique: true,
+        lowercase: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema );
