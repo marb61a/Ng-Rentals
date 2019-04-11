401248985eae8a35e87ebb75807b17d715fd02b9
marb61a <marb61a@gmail.com>
2019-04-11 15:52:32 +0100

    added functionality to auth guard

--- src/app/auth/shared/auth.guard.ts
+++ src/app/auth/shared/auth.guard.ts
@@ -9,7 +9,39 @@ export class AuthGuard implements CanActivate {
     constructor(private auth: AuthService,
         private router: Router) {}
 
+    private handleAuthState(): boolean {
+        if (this.isLoginOrRegister()) {
+            this.router.navigate(['/rentals']);
+            return false;
+        }
+      
+        return true;
+    }
+
+    private handleNotAuthState(): boolean {
+        if(this.isLoginOrRegister()) {
+            return true;
+        }
+
+        this.router.navigate(['/login']);
+        return false;
+    }
+
+    private isLoginOrRegister(): boolean {
+        if (this.url.includes('login') || this.url.includes('register')) {
+            return true;
+        }
+      
+        return false;
+    }
+    
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean{
         this.url = state.url;
+
+        if(this.auth.isAuthenticated()) {
+            return this.handleAuthState();
+        }
+
+        return this.handleNotAuthState();
     }
 }
